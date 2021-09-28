1b32f4f0c94ec640ab61642db1c4b00955d2c54b
Noa <s215793@dtu.dk>
2021-09-28 10:16:24 +0200

    9. commit development branch - spil

---
+++ src/Spil/Spil.java
@@ -0,0 +1,7 @@
+package Spil;
+
+public class Spil {
+
+
+    }
+
--- src/Spil/TerningeKast.java
+++ src/Spil/TerningeKast.java
@@ -1,10 +1,9 @@
 package Spil;
 
 public class TerningeKast {
-    public static void main (String[] args)
-    {
-        // Opretter et objekt til terningekast og ruller dem
 
+    public static void main (String[] args){
+        // Opretter et objekt til terningekast og ruller dem
         Terning terning1, terning2;
         int sum;
 
@@ -17,6 +16,8 @@ public class TerningeKast {
 
         sum = terning1.getAntalOjne() + terning2.getAntalOjne();
         System.out.println("Sum: " + sum);
+
+
 
 
     }
