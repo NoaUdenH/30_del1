1c6979f5d5e280af7384d87b032cb423b511b540
Noa <s215793@dtu.dk>
2021-09-28 09:06:55 +0200

    5. commit development branch - terning kommentarer

--- src/Spil/Terning.java
+++ src/Spil/Terning.java
@@ -1,23 +1,26 @@
 package Spil;
 
 public class Terning {
-    private final int MAX = 6; // maksimal antal øjne
+    private final int MAX = 6; // maksimal antal øjne på en terning
 
-    private int antalØjne; // terningens værdi
+    private int antalØjne; // den værdi terningen viser
 
 
     public Terning() {antalØjne = 1;} // start værdi for terningen
 
 
+    // generere kast med et tilfældigt antal øjne
     public int kast(){
         antalØjne = (int)(Math.random() * MAX) + 1;
         return  antalØjne;
     }
 
+    // get og set
     public void setAntalØjne (int værdi) { antalØjne = værdi; }
     public int getAntalØjne() { return antalØjne; }
 
 
+    // Repræsenterer terningen som en String
     public String toString(){
         String resultat = Integer.toString(antalØjne);
         return resultat;
