219d630acd433b68a7274a47553c61dae9b0176c
Noa <s215793@dtu.dk>
2021-09-28 08:28:02 +0200

    2. commit terning

--- src/Spil/Terning.java
+++ src/Spil/Terning.java
@@ -1,4 +1,25 @@
 package Spil;
 
 public class Terning {
+    private final int MAX = 6; // maksimal antal øjne
+
+    private int antalØjne; // terningens værdi
+
+
+    public Terning() {antalØjne = 1;} // start værdi for terningen
+
+
+    public int kast(){
+        antalØjne = (int)(Math.random() * MAX) + 1;
+        return  antalØjne;
+    }
+
+    public void setAntalØjne (int værdi) { antalØjne = værdi; }
+    public int getAntalØjne() { return antalØjne; }
+
+
+    public String toString(){
+        String resultat = Integer.toString(antalØjne);
+        return resultat;
+    }
 }
